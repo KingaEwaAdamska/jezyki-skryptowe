49c7993e41ec12c8477944fa337fefe0f6683a72
KingaEwaAdamska <kingaewa05@gmail.com>
2026-03-18 13:45:27 +0100

    review changes

--- lista2/proper_nouns_ratio.py
+++ lista2/proper_nouns_ratio.py
@@ -3,27 +3,27 @@ import sys, utils
 total_sentences = 0
 sentences_with_pn = 0
 
-def handle_sentence(sentence):
-    global total_sentences, sentences_with_pn
-
-    total_sentences += 1
-
+def check_condition(sentence):
     in_word = False
     is_first_word = True
-    has_proper_noun = False
 
     for c in sentence:
         if c.isalpha():
             in_word = True
 
             if not is_first_word and c.isupper():
-                has_proper_noun = True
+                return True
 
             is_first_word = False
         else:
             in_word = False
+    return False
 
-    if has_proper_noun:
+def handle_sentence(sentence):
+    global total_sentences, sentences_with_pn
+    total_sentences += 1
+
+    if check_condition(sentence):
         sentences_with_pn += 1
 
 def main():
