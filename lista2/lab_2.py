6dde521da574e17a052f6d7ebd6712e75e47312a
Roch Mykietów <63952397+loudsheep@users.noreply.github.com>
2026-03-18 12:05:54 +0100

    L2 - second part
    * feat: second part of task
    
    * feat: refactor lab_2.py to improve line cleaning and header processing

--- lista2/lab_2.py
+++ lista2/lab_2.py
@@ -1,21 +1,82 @@
 import sys
 
-def echo(sentence):
-    return "(" + sentence + ")"
 
-def main(process_sentence):
+def clean_line(line):
+    cleaned = ""
+    in_space = False
 
-    sentence = ""
+    for c in line:
+        if c.isspace():
+            if not in_space:
+                cleaned += " "
+                in_space = True
+        else:
+            cleaned += c
+            in_space = False
+
+    return cleaned.strip()
+
+
+def main():
+    header_buffer = ""
+    line_count = 0
+    empty_lines = 0
+    has_preable = False
+
+    current_line = ""
     c = sys.stdin.read(1)
 
-    while c:
-        sentence += c
+    # check na preambule
+    while c and line_count < 10:
+        if c == "\n":
+            line_count += 1
 
-        if c in ".":
-            print(process_sentence(sentence.strip()))
-            sentence = ""
+            if current_line.strip() == "":
+                empty_lines += 1
+            else:
+                empty_lines = 0
+            
+            header_buffer += current_line + "\n"
+            current_line = ""
+
+            if empty_lines == 2:
+                has_preable = True
+                break
+        else:
+            current_line += c
 
         c = sys.stdin.read(1)
 
+    if not has_preable:
+        temp_line = ""
+        for char in header_buffer:
+            if char == '\n':
+                if temp_line.strip() == "-----":
+                    return
+                print(clean_line(temp_line))
+                temp_line = ""
+            else:
+                temp_line += char
+
+    # content
+    line = current_line
+    while c:
+        c = sys.stdin.read(1)
+        if c == "\n" or not c:
+            if line.strip() == "-----":
+                return
+
+            print(clean_line(line))
+            line = ""
+        else:
+            line += c
+    
+    if line:
+        if line.strip() == "-----":
+            return
+        print(clean_line(line))
+
+
+
 if __name__ == "__main__":
-    main(echo)
+    main()
